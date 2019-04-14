30b82b8161734700623e613b5e600758df43f2da
Etsuna <47378314+Etsuna@users.noreply.github.com>
2019-04-15 01:26:05 +0200

    ajout du prog

---
+++ PROG/check.sh
@@ -0,0 +1,28 @@
+myfile='c:/Users/Etsuna/Documents/GitHub/Nier/TEXT/TALK_TEXT/TALK_A.JA.PS3.csv'
+fileedit='c:/Users/Etsuna/Documents/GitHub/Nier/TEXT/TALK_TEXT/TALK_A.JA.PS3.TKD'
+textacheck='c:/Users/Etsuna/Documents/GitHub/Nier/TEXT/TALK_TEXT/correction.txt'
+
+while IFS=";" read -r col1 col2 col3 col4
+do
+    echo "I got:$col1|$col2|$col3"
+
+    blinderer=$(sed 's/[.*…"]/\\&/g' <<< "$col1")
+    # echo "blinder = $blinderer"
+
+   	check=$(cat $fileedit | sed -e ''"${col3}"'p' -e '/\"$col2\"/!d' | cut -f16 -d$'\t')
+
+   	if [[ "$col2" == "$check" ]]
+   	then
+   		echo "Ligne $col3 ok"
+   	else
+   		echo "Ligne $col31 pas ok"
+   		echo "$col3" >> $textacheck
+   	fi
+
+    # sed -e 's/'"$word"'.*/'"$str"'/g'
+
+    # awk 'NR==$col3 {gsub( /$col1/, "$col2" )}; NR!=$col3 {print}' $fileedit
+
+    # awk 'NR==$col3 {print gensub(/$col1/,"col2"),1}; NR!=4 {print}' $fileedit
+
+done < $myfile
---
+++ PROG/routine.sh
@@ -0,0 +1,19 @@
+myfile='c:/Users/Etsuna/Documents/GitHub/Nier/TEXT/TALK_TEXT/TALK_A.JA.PS3.csv'
+fileedit='c:/Users/Etsuna/Documents/GitHub/Nier/TEXT/TALK_TEXT/TALK_A.JA.PS3.TKD'
+
+while IFS=";" read -r col1 col2 col3 col4
+do
+    echo "I got:$col1|$col2|$col3"
+
+    blinderer=$(sed 's/[.*…"]/\\&/g' <<< "$col1")
+    # echo "blinder = $blinderer"
+
+   	sed -i -b ''"$col3"'s|'"$col1"'|'"$col2"'|g' $fileedit
+
+    # sed -e 's/'"$word"'.*/'"$str"'/g'
+
+    # awk 'NR==$col3 {gsub( /$col1/, "$col2" )}; NR!=$col3 {print}' $fileedit
+
+    # awk 'NR==$col3 {print gensub(/$col1/,"col2"),1}; NR!=4 {print}' $fileedit
+
+done < $myfile
